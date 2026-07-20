d9bc7cfb3135e8332f02159952db7625c1428096
S.Rajitha <srajitha299@gmail.com>
2026-07-20 22:03:09 +0530

    Remove Angular-HOL

--- Week_6/Angular-HOL/1-Angular-HOL/SourceCode/src/app/pages/course-list/course-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-course-list',
-  imports: [],
-  templateUrl: './course-list.html',
-  styleUrl: './course-list.css',
-})
-export class CourseList {}
